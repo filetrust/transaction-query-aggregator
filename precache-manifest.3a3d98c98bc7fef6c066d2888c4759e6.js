89f10ee97ffff69be20efcbee9fd14c66f0fb10b
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2021-01-14 12:25:33 +0000

    Deploying to gh-pages from  @ 24d619b03fdd6c23a91b3d6935883b9688d357d0 🚀

---
+++ precache-manifest.3a3d98c98bc7fef6c066d2888c4759e6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bf00b8d0605f4b64790012cff16e7a",
+    "url": "/transaction-query-aggregator/index.html"
+  },
+  {
+    "revision": "8e4359cfc0fe21b97f4d",
+    "url": "/transaction-query-aggregator/static/css/2.ecb7c289.chunk.css"
+  },
+  {
+    "revision": "77976721dabcd7c24bcc",
+    "url": "/transaction-query-aggregator/static/css/main.080bad95.chunk.css"
+  },
+  {
+    "revision": "8e4359cfc0fe21b97f4d",
+    "url": "/transaction-query-aggregator/static/js/2.5294b346.chunk.js"
+  },
+  {
+    "revision": "cccc6d91b5e64164365f6c4135751a26",
+    "url": "/transaction-query-aggregator/static/js/2.5294b346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77976721dabcd7c24bcc",
+    "url": "/transaction-query-aggregator/static/js/main.4e144564.chunk.js"
+  },
+  {
+    "revision": "97665cccfefc34f3b104",
+    "url": "/transaction-query-aggregator/static/js/runtime-main.5b3eded0.js"
+  },
+  {
+    "revision": "2df329bc6f8e14a499990a534383f61d",
+    "url": "/transaction-query-aggregator/static/media/api.2df329bc.yaml"
+  },
+  {
+    "revision": "8503c64fa486375dc20b3c00aa2e250a",
+    "url": "/transaction-query-aggregator/static/media/download-icon-white.8503c64f.svg"
+  },
+  {
+    "revision": "6332dd8cb99e02f089de4f747566362c",
+    "url": "/transaction-query-aggregator/static/media/drop-grey.6332dd8c.svg"
+  },
+  {
+    "revision": "813b1258bed1257b91bcff7bf0282fbc",
+    "url": "/transaction-query-aggregator/static/media/drop-white.813b1258.svg"
+  },
+  {
+    "revision": "34536746f7b8c5b46d8675f1941e1dbc",
+    "url": "/transaction-query-aggregator/static/media/info-icon.34536746.svg"
+  },
+  {
+    "revision": "6c8e57277f41d456ad53ae6fcbc74b80",
+    "url": "/transaction-query-aggregator/static/media/logo.6c8e5727.svg"
+  },
+  {
+    "revision": "d41c631875217e983fe2051e2c62932e",
+    "url": "/transaction-query-aggregator/static/media/modal-close.d41c6318.svg"
+  },
+  {
+    "revision": "b01fa7f07a4c2e355eff2be5069f4c93",
+    "url": "/transaction-query-aggregator/static/media/montserrat-regular-webfont.b01fa7f0.woff2"
+  },
+  {
+    "revision": "e8b7d8acf640a69f838ca38c063e33ff",
+    "url": "/transaction-query-aggregator/static/media/montserrat-regular-webfont.e8b7d8ac.woff"
+  },
+  {
+    "revision": "9d9e09d7ae09f64a8a365e669105bedd",
+    "url": "/transaction-query-aggregator/static/media/roboto-regular-webfont.9d9e09d7.woff"
+  },
+  {
+    "revision": "d96d07c1e0f7cdf7a1988dcb0bf6e0e4",
+    "url": "/transaction-query-aggregator/static/media/roboto-regular-webfont.d96d07c1.woff2"
+  }
+]);
